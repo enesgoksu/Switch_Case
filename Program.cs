43202e24d8d90c1d0e68a42952213b774b5c951e
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-03-20 17:46:39 +0300

    program.cs değiştirildi

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+       int month = DateTime.Now.Month;
+       
+       //Expression
+       switch (month)
+       {
+            case 1:
+                Console.WriteLine("Ocak Ayındasınız");
+                break;
+            case 2:
+                Console.WriteLine("Şubat Ayındasınız");
+                break;
+            case 3:
+                Console.WriteLine("Mart Ayındasınız");
+                break;
+        
+            default:
+            break;
+        }
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimindesiniz");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar mevsimindesiniz");
+                    break;
+
+                default:
+                break;
+            }
+    }
+}
